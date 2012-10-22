4937500ec1686af09acdc70e6b86afae19112203
Stephen Shinn <shinnsm@rose-hulman.edu>
2012-10-21 20:02:13 -0400

    MiniProject 4

---
+++ MiniProject04/buttonBox.js
@@ -0,0 +1,92 @@
+// From Getting Started With node.js and socket.io 
+// http://codehenge.net/blog/2011/12/getting-started-with-node-js-and-socket-io-v0-7-part-2/
+"use strict";
+
+var http = require('http'),
+    url = require('url'),
+    fs = require('fs'),
+    exec = require('child_process').exec,
+    server,
+    connectCount = 0;	// Number of connections to server
+
+var pushTimer = 100;
+
+server = http.createServer(function (req, res) {
+// server code
+    var path = url.parse(req.url).pathname;
+    console.log("path: " + path);
+    switch (path) {
+    case '/':
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write('<h1>Hello!</h1>Try<ul><li><a href="/buttonBox.html">Button Box Demo</a></li></ul>');
+
+        res.end();
+        break;
+
+    default:		// This is so all the files will be sent.
+        fs.readFile(__dirname + path, function (err, data) {
+            if (err) {return send404(res); }
+//            console.log("path2: " + path);
+            res.write(data, 'utf8');
+            res.end();
+        });
+        break;
+
+    }
+});
+
+var send404 = function (res) {
+    res.writeHead(404);
+    res.write('404');
+    res.end();
+};
+
+server.listen(8081);
+
+// socket.io, I choose you
+var io = require('socket.io').listen(server);
+io.set('log level', 2);
+
+// on a 'connection' event
+io.sockets.on('connection', function (socket) {
+    var frameCount = 0;	// Counts the frames from arecord
+    var lastFrame = 0;	// Last frame sent to browser
+    console.log("Connection " + socket.id + " accepted.");
+//    console.log("socket: " + socket);
+
+    // now that we have our connected 'socket' object, we can 
+    // define its event handlers
+
+    // Make sure some needed files are there
+    // The path to the analog devices changed from A5 to A6.  Check both.
+    var ainPath = "/sys/devices/platform/omap/tsc/";
+//    if(!fs.existsSync(ainPath)) {
+//        ainPath = "/sys/devices/platform/tsc/";
+//        if(!fs.existsSync(ainPath)) {
+//            throw "Can't find " + ainPath;
+//        }
+//    }
+
+function pushData() {
+	fs.readFile(ainPath + "ain6", 'base64', function(err, data) {
+		if(err) throw err;
+		socket.emit('ain', data);
+	});
+
+	setTimeout(pushData, pushTimer);
+}
+
+    socket.on('disconnect', function () {
+        console.log("Connection " + socket.id + " terminated.");
+        connectCount--;
+        if(connectCount === 0) {
+        }
+        console.log("connectCount = " + connectCount);
+    });
+
+    connectCount++;
+    console.log("connectCount = " + connectCount);
+
+	pushData();
+});
+
